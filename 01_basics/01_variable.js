70c3931a18cc07912c6337795d5a9a440ea6de40
bhimanikrunal <mcac240009@gmail.com>
2024-09-05 11:16:17 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountid="112233"
+let accountname="krunal"
+var accountemail="k@email.com"
+accounticity="jaipur"
+
+
+//const in object
+const car={type:"toyota",color:"grey"}
+//add in array
+car.owner="krunal"
+console.log(car.owner)
+
+
+console.log(accountid)
+//outputin table view
+console.table([accountid,accountname,accountemail,accounticity])
